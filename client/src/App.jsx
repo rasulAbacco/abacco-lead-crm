75892e2bc630a3329e5cb8770660fe9b27ed675e
rasulAbacco <rasul.abacco@gmail.com>
2026-03-13 19:34:46 +0530

    Enable maintenance mode in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -32,7 +32,7 @@ import AdminEmailDomains from "./pages/AdminEmailDomains";
 import DealUpdates from "./pages/DealUpdates";
 import EmpDealReport from "./pages/EmpDealReport";
 
-const MAINTENANCE_MODE = false;
+const MAINTENANCE_MODE = true;
 
 
 function App() {
